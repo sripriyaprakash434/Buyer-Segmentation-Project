abbcfc917631ff261d79e3ca7fd08f97a6dfc26e
sripriyaprakash434 <145444280+sripriyaprakash434@users.noreply.github.com>
2026-06-18 20:36:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,17 @@ fig, ax = plt.subplots()
 clients["client_type"].value_counts().plot(kind="bar", ax=ax)
 
 st.pyplot(fig)
+
+st.subheader("Loan Applied Distribution")
+
+fig, ax = plt.subplots()
+
+clients["loan_applied"].value_counts().plot(
+    kind="pie",
+    autopct="%1.1f%%",
+    ax=ax
+)
+
+ax.set_ylabel("")
+
+st.pyplot(fig)
